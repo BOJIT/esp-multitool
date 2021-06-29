276c588eda76e01bf27f7c6a8bcd4c241cc9ca92
BOJIT <35117353+BOJIT@users.noreply.github.com>
2021-06-29 12:23:15 +0100

    Add commandline option select and serial initialisation code

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # esp32-multitool
 Tool for flashing/monitoring ESP-32s over ESP-NOW
+
+
+Command format:
+`esp-multitool.py --[global args] command --[command args]`
--- tools/esp-multitool.py
+++ tools/esp-multitool.py
@@ -8,7 +8,35 @@
 import os
 import sys
 import argparse
+import cutie
 
+# This block is taken straight from esptool.py
+try:
+    import serial
+except ImportError:
+    print("Pyserial is not installed for %s. Check the README for installation instructions." % (sys.executable))
+    raise
+
+# check 'serial' is 'pyserial' and not 'serial' https://github.com/espressif/esptool/issues/269
+try:
+    if "serialization" in serial.__doc__ and "deserialization" in serial.__doc__:
+        raise ImportError("""
+esp-multitool.py depends on pyserial, but there is a conflict with a currently installed package named 'serial'.""")
+except TypeError:
+    pass  # __doc__ returns None for pyserial
+
+try:
+    import serial.tools.list_ports as list_ports
+except ImportError:
+    print("The installed version (%s) of pyserial appears to be too old for esptool.py (Python interpreter %s). "
+          "Check the README for installation instructions." % (sys.VERSION, sys.executable))
+    raise
+except Exception:
+    if sys.platform == "darwin":
+        # swallow the exception, this is a known issue in pyserial+macOS Big Sur preview ref https://github.com/espressif/esptool/issues/540
+        list_ports = None
+    else:
+        raise
 
 
 __version__ = "0.0.1"
@@ -25,34 +53,78 @@ class ESPMultitool():
         ESP-Multitool hardware.
     """
 
-    BAUD_RATE = 115200
+    # When running this script as a module, disable any printout/interactive
+    # shell behaviour by default. Only set true by argparse.
+    TERMINAL = False
+
+    BAUD_RATE = 115200                  # Serial communication baud rate:
+                                        # can only be changed for OTA and Serial operations
+    DEFAULT_SERIAL_WRITE_TIMEOUT = 10   # timeout for serial port write
+    DEFAULT_CONNECT_ATTEMPTS = 7        # default number of times to try connection
+
+
+    def __init__(self, port=None):
+        self._port = None
+        if port is not None:
+            self.port(port)
+
+    # Auto port-creation code. If port exists, it is re-used.
+    def port(self, port=None):
+        if self._port is None:
+            print("Creating new Serial instance...")
+            if port is None:
+                # Show all ports and wait for user prompt (terminal only)
+                portlist = list_ports.comports()
+                ports = ([p.device for p in portlist])
+                port = self._option_prompt("Select Serial Port:", ports)
+
+                self._port = serial.Serial(port, self.BAUD_RATE)
+
+            elif isinstance(port, serial.Serial):
+                # Assign existing serial objet - for use as a module
+                self._port = port
+
+            elif isinstance(port, str):
+                # Open port based on serial identifier string
+                self._port = serial.Serial(port, self.BAUD_RATE)
+
+        return self._port
+
+
+    # For any settings that aren't preset, a terminal prompt is created. If
+    # the class is used in a module, an exception will occur.
+    def _option_prompt(self, msg, opts):
+        if self.TERMINAL is False:
+            raise Exception("Behaviour not explicit - Possible options: \n" + "\n".join(opts))
+        else:
+            print('\n' + msg + '\n')
+            opt = opts[cutie.select(opts)]
+            print('')
+            return opt
+
 
     # Sub-command functions
+    def control(self, args):
+        port = self.port()
 
-    def control(args):
-        pass
+    def discover(self, args):
+        port = self.port()
 
+    def flash(self, args):
+        port = self.port()
 
-    def discover(args):
-        pass
+    def serial(self, args):
+        port = self.port()
 
-
-    def flash(args):
-        pass
-
-
-    def serial(args):
-        pass
-
-
-    def stats(args):
-        pass
+    def stats(self, args):
+        port = self.port()
 
 
 
 def main(argv=None):
-    # Instantiate base class
+    # Instantiate base class with interactive terminal behaviour
     espm = ESPMultitool()
+    espm.TERMINAL = True
 
     # Argument parser
     parser = argparse.ArgumentParser(description='esp-multitool.py v%s - \
@@ -162,6 +234,9 @@ def main(argv=None):
         parser.print_help()
         sys.exit(0)
 
+    # Call relevant command
+    operation_func = getattr(espm, args.operation)
+    operation_func(args)
 
 
 if __name__ == '__main__':
